aa54461c175706c419274eba902cd6126826481a
premiumElevator <43154659+premiumElevator@users.noreply.github.com>
2018-10-22 16:27:18 -0400

    javadoc comments and average hits per month

--- LogAnalyzer.java
+++ LogAnalyzer.java
@@ -1,8 +1,8 @@
 /**
  * Read web server data and analyse hourly access patterns.
  *
- * @author David J. Barnes and Michael Kölling.
- * @version    2016.02.29
+ * @author Peter Basily
+ * @version    2018.10.22
  */
 public class LogAnalyzer
 {
@@ -22,15 +22,23 @@ public class LogAnalyzer
       // Create the array object to hold the hourly
       // access counts.
       hourCounts = new int[24];
-      dayCounts = new int[29];
+      dayCounts = new int[31];
       monthCounts = new int[13];
       yearCounts = new int[5];
       // Create the reader to obtain the data.
       reader = new LogfileReader();
    }
 
+  /**
+  *@param webLog the name of the file in "filename.txt" format
+  *
+  */
     public LogAnalyzer(String webLog)
     {
+
+      // Create the array object to hold the hourly, daily, monthly, and yearly
+      // access counts.
+
 
       hourCounts = new int[24];
       dayCounts = new int[29];
@@ -40,18 +48,26 @@ public class LogAnalyzer
       reader = new LogfileReader(webLog);
     }
 
+    /**
+    *@return numberOfAccesses returns total number of accesses using the monthCounts
+    *
+    */
+
     public int numberOfAccesses()
     {
         int total = 0;
 
-        for(int hour = 0; hour < hourCounts.length; hour++)
+        for(int month = 1; month < monthCounts.length; month++)
         {
-          total = total + hourCounts[hour];
+          total = total + monthCounts[month];
         }
 
         return total;
     }
-
+    /**
+    *totalAccessesPerMonth() method prints the month and the amount of visits
+    *used to analyze both specific year and total log
+    */
     public void totalAccessesPerMonth()
     {
 
@@ -62,6 +78,10 @@ public class LogAnalyzer
 
     }
 
+    /**
+    *@return busiestTwoHours() returns the total number of hits in the first of the 2 busiest hours
+    *as per book instructions.
+    */
 
     public int busiestTwoHours()
     {
@@ -79,7 +99,9 @@ public class LogAnalyzer
       return hourCounts[index];
     }
 
-
+    /**
+    @return busiestHour() returns an int value of the busiest hour used for both specific year or in general.
+    */
     public int busiestHour()
     {
       int index = 0;
@@ -94,6 +116,9 @@ public class LogAnalyzer
       return index;
     }
 
+    /**
+    @return quietestHour() returns an int value of the quietest hour used for both specific year or in general.
+    */
     public int quietestHour()
     {
       int index = busiestHour();
@@ -107,7 +132,9 @@ public class LogAnalyzer
       }
       return index;
     }
-
+    /**
+    @return busiestDay() returns an int value of the busiest day used for both specific year or general.
+    */
    public int busiestDay()
    {
       int index = 0;
@@ -122,6 +149,9 @@ public class LogAnalyzer
      return index;
    }
 
+   /**
+   @return quietestDay() returns an int value of the quietest day used for both specific year or general.
+   */
 
     public int quietestDay()
     {
@@ -129,23 +159,52 @@ public class LogAnalyzer
 
        for(int day = 1; day < dayCounts.length; day++)
        {
-        if(dayCounts[index] > dayCounts[day])
-        {
-           index = day;
-        }
+          if(dayCounts[index] > dayCounts[day])
+          {
+             index = day;
+          }
+       }
+       return index;
+    }
+    /**
+    @return busiestMonth() returns an int value of the busiest month used for both specific year or general.
+    */
+    public int busiestMonth()
+    {
+       int index = 1;
+
+       for(int month = 1; month < monthCounts.length; month++)
+       {
+           if(monthCounts[month] > monthCounts[index])
+           {
+               index = month;
+           }
+       }
+      return index;
+    }
+    /**
+    @return quietestMonth() returns an int value of the quietest month used for both specific year or general.
+    */
+    public int quietestMonth()
+    {
+       int index = busiestMonth();
+
+       for(int month = 1; month < monthCounts.length; month++)
+       {
+          if(monthCounts[index] > monthCounts[month])
+          {
+             index = month;
+          }
        }
        return index;
     }
 
 
     /**
-     * Analyze the hourly access data from the log file.
+    * Analyze the hourly access data from the log file.
+    *used only for total log file, not specific year.
      */
-    public void test()
-    {
 
-
-    }
     public void analyzeHourlyData()
     {
         while(reader.hasNext()) {
@@ -155,6 +214,11 @@ public class LogAnalyzer
         }
         reader.reset();
     }
+
+    /**
+    *Analyzes the daily access from a log file.
+    *used only for total log file, not specific year.
+    */
     public void analyzeDailyData()
     {
         while(reader.hasNext()) {
@@ -165,16 +229,55 @@ public class LogAnalyzer
         reader.reset();
     }
 
+  /**
+  * Analyzes the monthly access from a log file.
+  *used only for total log file, not specific year.
+  */
    public void analyzeMonthlyData()
    {
       while(reader.hasNext()) {
            LogEntry entry = reader.next();
            int month = entry.getMonth();
-           monthCounts[month+1]++;
+           monthCounts[month]++;
       }
       reader.reset();
    }
 
+   /**
+   *Analyzes specific year.
+   */
+   public void analyzeSpecificYear(int year)
+   {
+
+     while(reader.hasNext())
+     {
+       LogEntry entry = reader.next();
+
+       while(year == entry.getYear())
+       {
+         int month = entry.getMonth();
+         monthCounts[month]++;
+         int day = entry.getDay();
+         dayCounts[day]++;
+         int hour = entry.getHour();
+         hourCounts[hour]++;
+
+         if(!reader.hasNext())  //required for 2018, once it reaches the end of 2018, it will break instead of advance into null.
+         {
+           break;
+         }
+         entry = reader.next();
+       }
+
+     }
+     reader.reset();
+   }
+
+
+   /**
+   * Analyzes the yearly access from a log file.
+   *used only for total log file, not specific year.
+   */
    public void analyzeYearlyData()
    {
       while(reader.hasNext()) {
@@ -184,6 +287,18 @@ public class LogAnalyzer
       }
 
       reader.reset();
+   }
+   /**
+   * @return the average visits per month of a specific year.
+   */
+   public int averagePerMonthPerYear(int year)
+   {
+     analyzeSpecificYear(year);
+
+     int average = numberOfAccesses()/12;
+
+     return average;
+
    }
 
 
@@ -200,6 +315,12 @@ public class LogAnalyzer
         }
     }
 
+    /**
+     * Print the daily counts.
+     * These should have been set with a prior
+     * call to analyzedailyData.
+     *for testing purposes
+     */
     public void printDailyCounts()
     {
         System.out.println("Day: Count");
@@ -208,14 +329,22 @@ public class LogAnalyzer
         }
     }
 
+    /**
+     * Print the yearly counts.
+     * These should have been set with a prior
+     * call to analyzeYearlyData.
+     *for testing purposes
+     */
     public void printYearlyCounts()
     {
         System.out.println("year: Count");
-        int yearDisplay = 2014;
+        LogEntry entry = reader.next();
+        int yearDisplay = entry.getYear();
         for(int year = 0; year < yearCounts.length; year++) {
             System.out.println(yearDisplay + ": " + yearCounts[year]);
             yearDisplay++;
         }
+        reader.reset();
     }
     /**
      * Print the lines of data read by the LogfileReader
